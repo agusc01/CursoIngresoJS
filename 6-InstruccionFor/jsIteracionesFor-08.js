4dc399c0edb589c3d7a97902b3374079adf6c362
itisanibble <cacciatoriagustin@gmail.com>
2021-02-25 20:09:32 -0300

    parte a y b ejercicio de for del profe

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,166 +1,204 @@
 /*
 Alumno: Cacciatori, Agustín
-8)
-Enunciado:
-al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
+For 10 A-ingrese nombre , sexo , edad (validar que si es mujer debe ser adolescente y si el hombre debe ser niño), altura (validar), temperatura corporal
+
+For 10 B- necesitamos saber: el nombre y el sexo de la persona mas alta , --de las mujeres el nombre de la mas joven ,--de los hombre el nombre del mas bajito , ... solo si los hay
+
 */
 
 function mostrar()
 {
-	/*
-	var banderaEsPrimo; // parto de la idea que el número sera primo, soy optimista ;)
-	var cantidadDivisores;
-	var contador;
+	var nombre;
+	var sexo;
+	var edad;
+	var altura;
+	var temperatura;
+	var respuestaDeCorte;
+	//punto B
+	var banderaMasAlto;
+	var nombreMasAlto;
+	var sexoDelMasAlto;
+	var personaMasAlta;
+	var nombreMujerMasJoven;
+	var banderaMujerMasJoven;
+	var edadMujerMasJoven;
+	var nombreHombreMasBajo;
+	var banderaHombreMasBajo;
+	var alturaHombreMasBajo;
 	var mensaje;
 
-	banderaEsPrimo=true;
-	cantidadDivisores=0;
+	banderaMasAlto=true;
+	banderaMujerMasJoven=true;
+	banderaHombreMasBajo=true;
 
-	numeroIngresado=prompt("Ingrese un número mayor a 0");
-	numeroIngresado=parseInt(numeroIngresado);
-	while(isNaN(numeroIngresado)==true || numeroIngresado<1)
+
+	for(;;)
 	{
-		numeroIngresado=prompt("Error, ingrese un número mayor a 0");
-		numeroIngresado=parseInt(numeroIngresado);	
-	}
-
-	// numeroIngresado++;
-	for(contador=1;contador<numeroIngresado;contador++)
-	{
-
-		if(numeroIngresado%contador==0)
-		{
-			console.log(contador);
-			cantidadDivisores++;
-		}
-		if(cantidadDivisores>1)
-		{
-			banderaEsPrimo=false;
-			break;
-		}
-	}
-
-
-	if(banderaEsPrimo==true)
-	{
-		mensaje="El número ingresado "+numeroIngresado+" es primo";
-	}
-	else 
-	{
-		mensaje="El número ingresado "+numeroIngresado+" NO es primo";
-	}
-
-	console.log(mensaje);
-	console.log(cantidadDivisores);
-	alert(mensaje)
-	*/
-
-
-	var numeroIngresado;
-	var numeroAnterior;
-	var mensaje;
-	var limite;
-
-
-	numeroIngresado=prompt("Ingrese un número mayor a 0");
-	numeroIngresado=parseInt(numeroIngresado);
-	while(isNaN(numeroIngresado)==true || numeroIngresado<1)
-	{
-		numeroIngresado=prompt("Error, ingrese un número mayor a 0");
-		numeroIngresado=parseInt(numeroIngresado);	
-	}
-
-	limite=numeroIngresado/2;
-	for(numeroAnterior=2;numeroAnterior<limite;numeroAnterior++)
-	{
-		if(numeroIngresado%numeroAnterior==0)
-		{
-			break;
-		}
-
-	}
-
-
-	if(numeroAnterior>limite || numeroIngresado==2)
-	{
-		mensaje=numeroIngresado+" primo";
-	}
-	else 
-	{
-		mensaje=numeroIngresado+" no primo";
-	}
-	alert(mensaje)
-	console.log(mensaje);
-
-
-
-
-
-
-
-
-
-
-/*
-	
-	var numeroIngresado;
-	var numeroAnterior;
-	var limite;
-	var minimoNumero;
-
-	minimoNumero=0;
-
-	numeroIngresado=prompt("Ingrese un número mayor a 0");
-	numeroIngresado=parseInt(numeroIngresado);
-	while(isNaN(numeroIngresado)==true || numeroIngresado<1)
-	{
-		numeroIngresado=prompt("Error, ingrese un número mayor a 0");
-		numeroIngresado=parseInt(numeroIngresado);	
-	}
-
-	//minimoNumero esta por si quieren buscar entre dos números y no 
-	//entre el número ingresado y el 1
-	//nota: si yo se que minimoNumero>2 se podría sacar 1 if y 1 else
-	for(;numeroIngresado>minimoNumero;numeroIngresado--)
-	{
-
 		
-		//solo si es impar, NO existe números primos pares... EXCEPTO el 2 
-		if(numeroIngresado%2==1)
+		nombre=prompt("Ingrese el nombre: ");
+		while(isNaN(nombre)==false)
 		{
-			// limite=numeroIngresado/2;
-			limite=Math.sqrt(numeroIngresado);
-			for(numeroAnterior=2;numeroAnterior<limite;numeroAnterior++)
+			nombre=prompt("Error, ingrese el nombre: ");
+		}
+		
+		altura=prompt("Ingrese la altura: ");
+		altura=parseFloat(altura);
+		while(isNaN(altura)==true || altura<0.5 || altura>3)
+		{
+			altura=prompt("Error, Ingrese la altura: ");
+			altura=parseFloat(altura);
+		}
+
+		temperatura=prompt("Ingrese la temperatura: ");
+		temperatura=parseFloat(temperatura);
+		while(isNaN(temperatura)==true || temperatura<34 || temperatura>40)
+		{
+			temperatura=prompt("Error, Ingrese la temperatura: ");
+			temperatura=parseFloat(temperatura);
+		}
+
+
+		sexo=prompt("Ingrese el sexo: ('femenino' o 'masculino') ");
+		while(isNaN(sexo)==false || (sexo!='femenino' && sexo!='masculino'))
+		{
+			sexo=prompt("Error, ingrese el sexo: ");
+		}
+		
+		edad=prompt("Ingrese la edad: ");
+		edad=parseInt(edad);
+		if(sexo=='femenino')
+		{
+			while(isNaN(edad)==true || (edad<12 || edad>18))
 			{
-				if(numeroIngresado%numeroAnterior==0)
+	
+				edad=prompt("Error, ingrese la edad: ");
+				edad=parseInt(edad);
+			}
+			if(banderaMujerMasJoven==true)
+			{
+				banderaMujerMasJoven=false;
+				nombreMujerMasJoven=nombre;
+				edadMujerMasJoven=edad;
+			}
+			else
+			{
+				if(edad<edadMujerMasJoven)
 				{
-					break;
+					nombreMujerMasJoven=nombre;
+					edadMujerMasJoven=edad;
 				}
 			}
+
+		}
+		else//sexo=='masculino'
+		{
+			while(isNaN(edad)==true || edad>13)
+			{
+				edad=prompt("Error, ingrese la edad: ");
+				edad=parseInt(edad);
+			}
+			if(banderaHombreMasBajo==true)
+			{
+				banderaHombreMasBajo=false;
+				nombreHombreMasBajo=nombre;
+				alturaHombreMasBajo=altura;
+			}
+			else
+			{
+				if(altura<alturaHombreMasBajo)
+				{
+					nombreHombreMasBajo=nombre;
+					alturaHombreMasBajo=altura;
+				}
+			}
+		}//fin if(sexo=='f')
+	
+
+		if(banderaMasAlto==true)
+		{
+			banderaMasAlto=false;
+			nombreMasAlto=nombre;
+			sexoDelMasAlto=sexo;
+			personaMasAlta=altura;
 		}
 		else
 		{
-			if(numeroIngresado==2)//es primo
+			if(altura>personaMasAlta)
 			{
-				numeroAnterior=3;
-				limite=2;
+				nombreMasAlto=nombre;
+				sexoDelMasAlto=sexo;
+				personaMasAlta=altura;
 			}
-			else //no es primo, porque es número par y no es 2
-			{
-				numeroAnterior=2;
-				limite=3;
-			}
-		}
+		}//fin if(banderaMasAlto==true)
 
-		if(numeroAnterior>limite)
+
+
+		respuestaDeCorte=prompt("Si quiere seguir ingresar 'si' ");
+		if(respuestaDeCorte!='si')
 		{
-			console.log(numeroIngresado);//esto imprime el número primo
+			break;
 		}
 
+	}//fin for(;respuestaDeCorte='si';)
+
+	/*For 10 B- necesitamos saber: el nombre y el sexo de la persona mas alta , --de las mujeres el nombre de la mas joven ,--de los hombre el nombre del mas bajito , ... solo si los hay*/
+
+	mensaje="La persona más alta se llama: "+nombre+", y su sexo es "+sexo+", mide "+personaMasAlta+" metros";
+	alert(mensaje);
+
+	if(banderaMujerMasJoven==false)
+	{
+		mensaje="La mujer más joven tiene "+edadMujerMasJoven+" años"+" y su nombre es "+nombreMujerMasJoven;
 	}
+	else
+	{
+		mensaje="No se ingresaron mujeres";
+	}
+	alert(mensaje);
 
+	if(banderaHombreMasBajo==false)
+	{
+		mensaje="El hombre más bajo mide "+alturaHombreMasBajo+" metros y su nombre es "+nombreHombreMasBajo;
+	}
+	else
+	{
+		mensaje="No se ingresaron hombres";
+	}
+	alert(mensaje);
 
+	/*
+	TEST
+	agustin
+	2.5 altura
+	35 temperatura
+	masculino
+	10 edad
+
+	florencia
+	2.4 altura
+	40 temperatura
+	femenino
+	15 edad
+
+	mayra
+	2.1 altura
+	34 temperatura
+	femenino
+	17 edad
+
+	martin
+	2.7
+	38
+	masculino
+	8
+
+	La persona más alta se llama: martin, y su sexo es masculino, mide 2.7 metros
 	
-*/
+	La mujer más joven tiene 15 años y su nombre es florencia
+
+	El hombre más bajo mide 2.5 metros y su nombre es agustin
+
+	*/
+
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor10-25-02-20.js
+++ 6-InstruccionFor/jsIteracionesFor10-25-02-20.js
@@ -1,5 +1,3 @@
-
-
 function mostrar()
 {
 	var contador;
